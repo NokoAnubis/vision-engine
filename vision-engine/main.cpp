82c3737d598346583def0f08cd69e94c9b05011f
Joel Joseph <joeljordanjoseph@gmail.com>
2023-06-05 21:57:58 -0600

    Initial Commit

---
+++ vision-engine/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  vision-engine
+//
+//  Created by Joel Joseph on 6/5/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Vision Engine\n";
+    return 0;
+}
